e69aa327d163f7d22f19dda9c76cd090af49bfa9
pzmrzy <pzmrzy@gmail.com>
2013-10-25 23:54:12 +0800

    完善sqlcommand 继续写catalog

--- minisql/catalog.cpp
+++ minisql/catalog.cpp
@@ -9,3 +9,31 @@ catalog::catalog(void)
 catalog::~catalog(void)
 {
 }
+
+bool catalog::init(){
+	fstream fout;
+	fout.open(CATA,ios::out | ios::binary);
+
+	tableNum t(0);
+	return writeHead(fout, t);
+}
+
+bool catalog::writeHead(fstream& f, tableNum& t)
+{
+	f.write((char *)(t.getnum()), sizeof(int));
+    f.flush();
+}
+
+catainfo catalog::creat_Table(SqlCommand& cmd){
+	string dbname = cmd.getDatabaseName();
+	bool existdb = exist_Database(dbname);
+	if (!existdb)
+		return catainfo(false, "Database " + dbname + " Do Not Exist!");
+
+	string tname = cmd.getTableName();
+	bool existt = exist_Table(dbname, tname);
+	if (existt)
+		return catainfo(false, "Table " + tname + " Has Already Exist!");
+
+
+}
--- minisql/minisql.h
+++ minisql/minisql.h
@@ -4,23 +4,59 @@
 #include <string>
 #include <iostream>
 #include <vector>
-
+#include <fstream>
 using namespace std;
 
 //catalog返回的信息
 class catainfo{
 public:
+	//Ctor
+	catainfo(bool f, string mes){
+		succ = f;
+		message = mes;
+	}
+	//获取指令是否成功
+	bool getsucc(){
+		return succ;	
+	}
+	//输出错误信息
+	void print(){
+		cout<<message<<endl;
+	}
+private:
 	bool succ;
 	string message;
+
 };
 
 //属性信息
 class attribute{
-	string attr;	//名称
+public:
+	string name;	//名称
 	int datatype;	//数据类型
 	bool PK;		//主键
-	bool UNIQUE;	//唯一
+	bool UN;		//唯一
 	bool NN;		//非空
 };
 
+class table{
+public:
+	string name;					//名称
+	int attrNum;					//属性数
+	int recLength;					//记录长度
+	vector<attribute> attrList;		//属性列表
+}
+
+class tableNum{
+public:
+	tableNum(int n){
+		num = n;
+	}
+
+	int getnum(){
+		return num;
+	}
+private:
+	int num;
+}
 #endif
