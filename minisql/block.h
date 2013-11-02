499a115b26243f79af96d7406c1c68aadc96aa74
tgmerge <tgmgsp@gmail.com>
2013-11-02 22:15:14 +0800

    确定buffer的一些方法

--- minisql/block.h
+++ minisql/block.h
@@ -16,17 +16,40 @@ using namespace std;
 #define BLOCK_LEN 4096
 
 class Block {
-public:
-	string fileName;
-	int offset;
-	char content[BLOCK_LEN];
-	bool dirty;
-	bool active; // for lru
-	int value;   // for lru
 
 public:
+	string fileName;			// 文件名(数据库名)
+	string tableName;			// 表名
+	int offset;					// block在文件中的偏移量
+	char content[BLOCK_LEN];	// 数据
+	bool dirty;					// 是否需要写回文件
+	bool active;				// for lru
+	int value;					// for lru
+
+public:							// 构造，析构
+	// 初始化一个新块, fileName, tableName, offset均需设置
 	Block();
-	string getContentFrom(int start, int length);
+
+	// 如果dirty，先写回文件再销毁
+	~Block();
+
+private:						// 自用方法，读写
+	// 从某文件读一个block
+	Block read(string fileName, int offset);
+
+	// 将block写到文件
+	bool write();
+	
+public:							// RecordManager使用
+	// 返回tableName的所有块
+	static vector<Block> getTableBlocks(string tableName);
+
+	// 返回tableName的某个块（offset从IndexManager取得？）
+	static Block getTableBlock(string tableName, int offset);
+
+public:							// IndexManager使用
+	// 返回indexName的所有块
+	vector<Block> getIndexBlocks(string IndexName);
 };
 
 #endif
